f2a2788109fc8ca99c304bfd9c3843bc9dbf415d
dafalifia <dalianisyaul@gmail.com>
2023-11-14 11:09:24 +0700

    resto_dalia

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplikasi Resto Well</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Resto Well</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Hot Promo</a></li>
+                <li><a href="#">Makanan</a></li>
+                <li><a href="#">Minuman</a></li>
+                <li><a href="#">Desert</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="menu">
+        <h2>Menu Resto Well</h2>
+            <div class="menu-item">
+                <img src="img/bakso mercon.jpg" alt="menu 1">
+                <h3>Bakso Mercon</h3>
+                <p>
+                    Dekripsi tentang makanan misalnya nilai gizi dsb.
+                </p>
+                <p class="harga">Harga : Rp. 10.000</p>
+                <button class="order-button">Pesan</button>
+            </div>
+            <div class="menu-item">
+                <img src="img/bakso ori.jpg" alt="menu 2">
+                <h3>Bakso Ori</h3>
+                <p>
+                    Dekripsi tentang makanan misalnya nilai gizi dsb.
+                </p>
+                <p class="harga">Harga : Rp. 10.000</p>
+                <button class="order-button">Pesan</button>
+            </div>
+    </section >
+
+    <section class="contact">
+        <h2>Hubungi Kami</h2>
+        <p>Jika ada pertanyaan, saran atau kritik hubungi kami di:</p>
+        <p>WA 0812345
+        <p>Email : admin@gmail.com</p>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Restoran Well .Hak Cipta di Lindungi</p>
+    </footer>
+</body>
+</html>
